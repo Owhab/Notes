5033719f7b3825bd59cf5bab7235e591013d08d9
Abdul Owhab <owhab.cse@gmail.com>
2024-01-02 09:30:22 +0600

    working on screens setup

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/screens/Home";
+import SignIn from "./src/screens/SignIn";
+import SignUp from "./src/screens/SignUp";
+import Create from "./src/screens/Create";
+import Edit from "./src/screens/Edit";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const user = false;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {user ? (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Create" component={Create} />
+            <Stack.Screen name="Edit" component={Edit} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="SignIn"
+              component={SignIn}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="SignUp" component={SignUp} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,10 +1,36 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SignIn() {
   return (
-    <View>
+    <SafeAreaView>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.signInImage}
+          source={require("../../assets/images/empty-state.jpg")}
+        />
+      </View>
       <Text>SignIn</Text>
-    </View>
+    </SafeAreaView>
   );
 }
+
+const dimensions = Dimensions.get("window");
+const imageHeight = Math.round((dimensions.width * 9) / 16);
+// const imageWidth = dimensions.width;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  signInImage: {
+    flex: 1,
+    height: 800,
+    objectFit: "contain",
+  },
+});
